3975c8d8eb419c7604c424b9ef31cf1374074d68
Amit Dube <amit.dube.ms@gmail.com>
2022-04-19 17:16:58 +0530

    changes to return image response

--- server/routes/images.js
+++ server/routes/images.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import * as fs from 'fs';
+import * as path from 'path';
 
 var router = express.Router();
 
@@ -18,6 +19,35 @@ router.get('/:userId', async (req, res, next) => {
         res.status(200).send(listOfFiles);
     } catch (err) {
         res.status(500).send({ message: "User get image file names!", err: err });
+    }
+});
+
+router.get('/:userId/:imageFileName', async (req, res, next) => {
+    try {
+        let requestedImageFileName = req.params.imageFileName;
+        if (!requestedImageFileName) {
+            res.status(404).send('The requested resource was not found');
+            return;
+        }
+        let imageFolder = `public/images/${req.params.userId}`;
+        let imageFilePath = ''
+        let fileNames = fs.readdirSync(imageFolder);
+        for (let file of fileNames) {
+            if (requestedImageFileName == file) {
+                imageFilePath = `../../public/images/${req.params.userId}/` + file;
+                break;
+            }
+        }
+        if (imageFilePath) {
+            let absImageFilePath = path.resolve(__dirname, imageFilePath)
+            res.sendFile(absImageFilePath);
+        } else {
+            res.status(404).send('The requested resource was not found');
+        }
+
+    } catch (err) {
+        console.log(err)
+        res.status(500).send({ message: "Unable to get image!", err: err });
     }
 });
 
