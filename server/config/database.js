bf013b145c6cd72afa4b31274dcf366dafcc4a22
Amit Dube <amit.dube.ms@gmail.com>
2022-05-01 16:34:26 +0530

    changes to read ip address from config file

--- server/config/database.js
+++ server/config/database.js
@@ -1,5 +1,8 @@
-import Sequelize from 'sequelize';
-import { Config } from './config'
+import Sequelize, { json } from 'sequelize';
+import { Config } from './config';
+import * as fs from 'fs';
+
+const configPath = '/home/ec2-user/config/db.json';
 
 let pool = {
     max: 5,
@@ -7,8 +10,16 @@ let pool = {
     acquire: 30000,
     idle: 10000
 }
-
 let config = new Config('localhost', 'root', 'root', 'testdb', 'mysql', pool)
+if (fs.existsSync(configPath)) {
+    let jsonString = fs.readFileSync(configPath, {
+        encoding: 'utf-8'
+    });
+    const dbConfig = JSON.parse(jsonString);
+    config = new Config(dbConfig.host, dbConfig.user, dbConfig.password, dbConfig.database, 'mysql', pool)
+}
+
+
 
 const sequelize = new Sequelize(
     config.db,
